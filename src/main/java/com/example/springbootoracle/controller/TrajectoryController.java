c3b08f0ae1104a8ba953162ca8de50ed1922fab1
Kristijan Bošković <kristijan.boskovic@fer.hr>
2019-05-27 21:48:14 +0200

    Fixed table data display

--- src/main/java/com/example/springbootoracle/controller/TrajectoryController.java
+++ src/main/java/com/example/springbootoracle/controller/TrajectoryController.java
@@ -3,34 +3,25 @@ package com.example.springbootoracle.controller;
 import com.example.springbootoracle.entity.Trajectory;
 import com.example.springbootoracle.service.TrajectoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class TrajectoryController {
     @Autowired
     TrajectoryService trajectoryService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Trajectory> getAllTrajectories() {
-        return trajectoryService.getAllTrajectories();
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+//    public List<Trajectory> getAllTrajectories() {
+//        return trajectoryService.getAllTrajectories();
+//    }
+    public String getAllTrajectories(Model model) {
+        List<Trajectory> trajList = trajectoryService.getAllTrajectories();
+        model.addAttribute("trajList", trajList);
+        return "index";
     }
-//    public String getAllTrajectories(Model model) {
-//        model.addAttribute("trajectories", trajectoryService.getAllTrajectories());
-//        return "trajectories";
-//    }
-
-//    @RequestMapping(value = "/adduser", method = RequestMethod.POST,
-//            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseBody()
-//    public Trajectory addNewUser(@RequestBody Trajectory user) {
-//        return this.userService.addUser(user);
-//    }
-
-    //other controllers omitted for brevity
-
 }
